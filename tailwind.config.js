4bd0a3db91d85ec8946c00b1827184c5fd559794
Martin Häusler <hello@nightcoding.de>
2022-04-08 12:38:13 +0200

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        highlight: '#f46500',
+        secondary: '#4d7701',
+        dark: '#1b2c00',
+        creme: '#f4f0f4',
+      }
+    },
+  },
+  plugins: [],
+}
